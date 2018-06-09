a278a72ea75f04b01eddd9da24bc5acb3acc9bb7
Unknown <jme919@gmail.com>
2018-06-09 19:56:46 -0400

    add get routes

--- config/routes.js
+++ config/routes.js
@@ -1,3 +1,15 @@
+//bring in scrape function
+var scrape = require("../scripts/scrape");
+
+//bring in headlines and notes from the controller
+var headlinesController = require("..controllers/headlines");
+var notesController = require("../controllers.notes");
+
+
+
+
+
+
 module.exports = function(router) {
     //this route render the homepage
     router.get("/", function(req,res){
@@ -9,6 +21,28 @@ module.exports = function(router) {
         res.render("saved");
 
     });
-
     
+    router.get("/api/fetch", function(req, res){
+        headlinesController.fetch(function(err, docs){
+            if(!docs || docs.insertedCount === 0){
+                res.json({
+                    message: "No new articles right now, check back later!"
+                });
+            }
+            else{
+                res.json({
+                    message: "Added " + docs.insertedCount + " new articles!"
+                });
+            }
+        });
+    });
+    router.get("/api/headlines", function(req, res){
+        var query = {};
+        if (req.query.saved){
+            query = req.query;
+        }
+        headlinesController.get(query, function(data){
+            res.json(data);
+        });
+    });
 }
