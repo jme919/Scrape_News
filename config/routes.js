9fdfe8da021c9b20cce4148eee6b554f91fee46d
Unknown <jme919@gmail.com>
2018-06-09 20:52:30 -0400

    add functions to index.js

--- config/routes.js
+++ config/routes.js
@@ -62,4 +62,31 @@ module.exports = function(router) {
         });
     });
 
+    router.get("/api/notes/:headline_id?", function(req, res){
+        var query = {};
+        if(req.params.headline_id){
+            query._id = req.params.headline_id;
+        }
+        notesController.get(query, function(err, data){
+            res.json(data);
+        });
+    });
+
+    router.delete("/api/notes/:id", function(req, res){
+        var query = {};
+        query._id = req.params.id;
+        notesController.delete(query, function(err, data){
+            res.json(data);
+
+        });
+    });
+
+    router.post("/api/notes", function(req, res){
+        notesController.save(req.body, function(data){
+            res.json(data);
+        });
+    });
+
+
+
 }
---
+++ public/assets/javascript/index.js
@@ -0,0 +1,82 @@
+$(document).ready(function(){
+    var articleContainer = $(".card-content");
+    $(document).on("click", ".save", handleArticleSave);
+    $(document).on("click", ".scrape-new", handleArticleScrape);
+//run init page function once the page is ready
+    initPage();
+
+    function initPage() {
+        //Empty the article container, run an ajax request for any unsave articles
+        articleContainer.empty();
+        $.get("/api/headlines?saved=false")
+            .then(function(data){
+                //If there are new headlines, render them to the page
+                if(data && data.length){
+                    renderArticles(data);
+                }
+                else{
+                    //instead render a message explaining there are no articles
+                    renderEmpty();
+                }
+            });
+        
+    }
+
+    function renderArticles(articles){
+        //handles appending html containing article data to the page
+        var articleCards = [];
+        for(var i = 0; i< articles.length; i++){
+            articleCards.push(articles[i]);
+        }
+        articleContainer.append(articleCards);
+    }
+
+    function createCards(article){
+        var card = 
+            $(["<div class='columns'>",
+                "<div class='column'>",
+                "<div class='card'>",
+                "<div class='card-content has-text-centered'>",
+                "<span class='icon is-medium'>",
+                "<i class='fas fa-lg fa-newspaper'>", "</i>",
+                "</span>",
+
+                "<h2 class='title' id='headline'>", article.headline, "</h2>",
+                "<h3 class='subtitle' id='summary'>", article.summary, "</h3>",
+                "<a href='' class='button '>", "Go To The Source", "</a>",
+                "<a href='' class='button save'>", "Save For Later", "</a>",
+
+                "</div>",
+                "</div>",
+                "</div>",
+                "</div>"
+
+        ].join(""));
+        
+        card.data("_id", article._id);
+        return card;
+    }
+
+    function renderEmpty(){
+        var emptyAlert = 
+            $(["<div class='columns'>",
+                "<div class='column'>",
+                "<div class='card'>",
+                "<div class='card-content has-text-centered'>",
+                "<span class='icon is-medium'>",
+                "<i class='fas fa-lg fa-newspaper'>", "</i>",
+                "</span>",
+
+                "<h2 class='title' id='headline'>", "Whoops, there are no new articles", "</h2>",
+                "<h3 class='subtitle' id='summary'>", "What would you like to do?", "</h3>",
+                "<a href='/saved' class='button is-large>","Go To Saved Articles","</a>",
+                "<a class='button is-large scrape-new'>","Try to Scrape Again","</a>",
+
+                "</div>",
+                "</div>",
+                "</div>",
+                "</div>"
+
+        ])
+    }
+})
